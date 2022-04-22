0a57ce07c6fd2c55826f71a8273374eadde91ae4
isha <ishadixit642@gmail.com>
2022-04-23 00:06:44 +0530

    add carousel

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+  res.send("\index.html");
+});
 
 app.listen(3000,function(req,res){
   console.log("Successfully started at 3000");
